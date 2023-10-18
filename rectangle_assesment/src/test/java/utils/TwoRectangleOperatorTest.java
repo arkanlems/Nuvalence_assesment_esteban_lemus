197f5819e8a0df7858ee564952aee57696064dc3
arkanlems <estebanlemuspena@gmail.com>
2023-10-18 01:47:08 -0500

    Added the logic of adjacency with the test pass

--- rectangle_assesment/src/test/java/utils/TwoRectangleOperatorTest.java
+++ rectangle_assesment/src/test/java/utils/TwoRectangleOperatorTest.java
@@ -37,16 +37,40 @@ public class TwoRectangleOperatorTest {
         assertEquals(TwoRectangleOperator.HEIGHT_WIDTH_INTERSECTION,TwoRectangleOperator.itsIntersection(rectangleA,rectangleB));
     }
     @Test
-    public void itsContained(){
+    public void itsContainedTest(){
         rectangleA = createRectangle(1,3.5,5,1);
         rectangleB = createRectangle(1.5,3,4,2);
         assertEquals(TwoRectangleOperator.ITS_CONTAINMENT,TwoRectangleOperator.itsCointeinment(rectangleA,rectangleB));
     }
     @Test
-    public void notContained(){
+    public void notContainedTest(){
         rectangleA = createRectangle(1,3.5,5,1);
         rectangleB = createRectangle(0.5,3,4,2);
         assertEquals(TwoRectangleOperator.NOT_CONTAINMENT,TwoRectangleOperator.itsCointeinment(rectangleA,rectangleB));
+    }
+    @Test
+    public void adjacencySubLineTest(){
+        rectangleA = createRectangle(3.5,3.5,5,2.5);
+        rectangleB = createRectangle(2,4,3.5,1.5);
+        assertEquals(TwoRectangleOperator.ADJACENCY_SUB_LINE,TwoRectangleOperator.itsAdjecent(rectangleA,rectangleB));
+    }
+    @Test
+    public void adjacencyProperTest(){
+        rectangleA = createRectangle(2,4,3.5,1.5);
+        rectangleB = createRectangle(3.5,4,5,1.5);
+        assertEquals(TwoRectangleOperator.ADJACENCY_PROPER,TwoRectangleOperator.itsAdjecent(rectangleA,rectangleB));
+    }
+    @Test
+    public void adjacencyPartialTest(){
+        rectangleA = createRectangle(2,4,3.5,1.5);
+        rectangleB = createRectangle(3.5,5,4.5,2.5);
+        assertEquals(TwoRectangleOperator.ADJACENCY_PARTIAL,TwoRectangleOperator.itsAdjecent(rectangleA,rectangleB));
+    }
+    @Test
+    public void NoAdjacencyTest(){
+        rectangleA = createRectangle(2,4,3.5,1.5);
+        rectangleB = createRectangle(4,5,5,2.5);
+        assertEquals(TwoRectangleOperator.NO_ADJACENCY,TwoRectangleOperator.itsAdjecent(rectangleA,rectangleB));
     }
     private Rectangle createRectangle(double pointAx, double pointAy, double pointDx, double pointDy){
         return new Rectangle(new Point2D.Double(pointAx,pointAy),new Point2D.Double(pointDx,pointDy));
